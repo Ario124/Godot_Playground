4ab9b1c5f24adfe7258970957daa4542b4a260d5
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-11-21 11:11:20 +0100

    Added containers for square particles effect with button to activate

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -1,11 +1,12 @@
 extends Control
 
-
+onready var square_particles_container = $HBoxContainer/FirstBox/CenterContainer
 var square_particles = load("res://Scenes/SquareParticles.tscn")
-
 
 func _on_Button_pressed():
 	var particles = square_particles.instance()
-	add_child(particles)
+	particles.scale.x = 0.4
+	particles.scale.y = 0.4
+	square_particles_container.add_child(particles)
 	yield(get_tree().create_timer(5.0), "timeout")
-	remove_child(particles)
+	square_particles_container.remove_child(particles)
