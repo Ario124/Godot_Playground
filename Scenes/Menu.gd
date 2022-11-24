0f26e8e1df6024bb2680e274f4b1b678146bc0df
Ario124 <47329632+Ario124@users.noreply.github.com>
2022-11-24 19:33:47 +0100

    Added portal particles to main scene. Also added some more testing scenes

--- Scenes/Menu.gd
+++ Scenes/Menu.gd
@@ -2,8 +2,10 @@ extends Control
 
 onready var square_particles_container = $HBoxContainer/FirstBox/CenterContainer
 onready var follow_particles_container = $HBoxContainer/FirstBox2/CenterContainer
+onready var portal_particles_container = $HBoxContainer/FirstBox3/CenterContainer
 var square_particles = load("res://Scenes/SquareParticles.tscn")
 var follow_particles = load("res://Scenes/ParticlePath.tscn")
+var portal_particles = load("res://Scenes/Portal.tscn")
 
 func _on_Button_pressed():
 	var particles = square_particles.instance()
@@ -23,3 +25,10 @@ func _on_Follow_pressed():
 	follow_particles_container.remove_child(particles)
 
 
+
+
+func _on_Portal_pressed():
+	var particles = portal_particles.instance()
+	portal_particles_container.add_child(particles)
+	yield(get_tree().create_timer(10.0), "timeout")
+	portal_particles_container.remove_child(particles)
